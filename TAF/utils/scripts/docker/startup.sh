d1223df12c25ff70b8d75ddc61e7d31839f21d97
Bruce <weichou1229@gmail.com>
2019-09-27 09:57:04 +0100

    Add Jenkinsfile
    Signed-off-by: Bruce <weichou1229@gmail.com>

--- TAF/utils/scripts/docker/startup.sh
+++ TAF/utils/scripts/docker/startup.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 docker run --rm -v ${WORK_DIR}:${WORK_DIR} -w ${WORK_DIR} -v /var/run/docker.sock:/var/run/docker.sock   \
-        docker/compose:1.24.0 -f "${WORK_DIR}/TAF/utils/scripts/docker/docker-compose.yaml" up -d
+        docker/compose:1.24.0 -f "${WORK_DIR}/TAF/utils/scripts/docker/docker-compose.yaml" up -d $*
