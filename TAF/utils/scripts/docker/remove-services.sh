2ad9427985eba5f467b41ed74d18604fe1b8909c
Cherry Wang <cherry@iotechsys.com>
2020-06-17 11:13:06 +0800

    Add --security-opt label:disable in docker run command
    Signed-off-by: Cherry Wang <cherry@iotechsys.com>

--- TAF/utils/scripts/docker/remove-services.sh
+++ TAF/utils/scripts/docker/remove-services.sh
@@ -10,6 +10,7 @@ if [ $1 == "-backward" ]; then
 fi
 
 docker run --rm -v ${WORK_DIR}:${WORK_DIR}:rw,z -w ${WORK_DIR} -v /var/run/docker.sock:/var/run/docker.sock \
-        --env-file ${WORK_DIR}/TAF/utils/scripts/docker/${ARCH}.env --env WORK_DIR=${WORK_DIR} --env PROFILE=${PROFILE}  \
+        --env-file ${WORK_DIR}/TAF/utils/scripts/docker/${ARCH}.env --security-opt label:disable \
+        --env WORK_DIR=${WORK_DIR} --env PROFILE=${PROFILE}  \
         --env DS_PROFILE=${DS_PROFILE} --env CONF_DIR=${CONF_DIR} --env REGISTRY_URL=${REGISTRY_URL} \
         ${COMPOSE_IMAGE} -f "${WORK_DIR}/TAF/utils/scripts/docker/docker-compose${BACKWARD}.yaml" rm -s -f -v $*
