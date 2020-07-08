84241dad37b3dba6b458feaa77c623deeb5b1ded
lenny <leonard.goodell@intel.com>
2020-07-07 17:41:01 -0700

    refactor: Updated scripts to use curl and pull from new `source` folder.
    Signed-off-by: lenny <leonard.goodell@intel.com>

--- TAF/utils/scripts/docker/get-compose-file-backward.sh
+++ TAF/utils/scripts/docker/get-compose-file-backward.sh
@@ -6,9 +6,6 @@ USE_ARCH=${2:--x86_64}
 USE_SECURITY=${3:--}
 USE_RELEASE=${4:-geneva}
 
-# so wget on windows can pull files
-[ "$(uname -o)" = "Msys" ] && WINDOWS_WGET_OPTION="--no-check-certificate"
-
 # # x86_64 or arm64
 [ "$USE_ARCH" = "arm64" ] && USE_ARM64="-arm64" && ARM64_OPTION="arm64"
 
@@ -22,9 +19,10 @@ USE_RELEASE=${4:-geneva}
 mkdir temp
 
 # Use base compose file from nightly-build for core services
-wget ${WINDOWS_WGET_OPTION} -O temp/nb-compose.yaml "https://raw.githubusercontent.com/lenny-intel/developer-scripts/multi2/releases/nightly-build/compose-files/docker-compose-nexus-base.yml"
+# TODO: Change URl use edgexfoundry master once developer-scripts PR is merged
+curl -o temp/nb-compose.yaml "https://raw.githubusercontent.com/lenny-intel/developer-scripts/multi2/releases/nightly-build/compose-files/source/docker-compose-nexus-base.yml"
 
-wget ${WINDOWS_WGET_OPTION} -O temp/geneva-compose.yaml "https://raw.githubusercontent.com/edgexfoundry/developer-scripts/master/releases/geneva/compose-files/docker-compose-geneva${USE_DB}${USE_NO_SECURITY}${USE_ARM64}.yml"
+curl -o temp/geneva-compose.yaml "https://raw.githubusercontent.com/edgexfoundry/developer-scripts/master/releases/geneva/compose-files/docker-compose-geneva${USE_DB}${USE_NO_SECURITY}${USE_ARM64}.yml"
 
 # replace geneva core services with nightly-build core services
 sed -n '/metadata:/,/scheduler:/{//!p;}' temp/nb-compose.yaml > temp/core-services.yaml
@@ -48,6 +46,5 @@ fi
 sed -i 's/\${CORE_EDGEX_REPOSITORY}/nexus3.edgexfoundry.org:10004/g' docker-compose-backward.yaml
 sed -i 's/\${CORE_EDGEX_VERSION}/master/g' docker-compose-backward.yaml
 sed -i 's/\${DEV}//g' docker-compose-backward.yaml
-sed -i 's/\- database/- redis/g' docker-compose-backward.yaml
 
 rm -rf temp
--- TAF/utils/scripts/docker/sync-nightly-build.sh
+++ TAF/utils/scripts/docker/sync-nightly-build.sh
@@ -2,7 +2,7 @@
 
 # TODO: Change URl and file spec to use edgexfoundry master once developer-scripts PR is merged
 NIGHT_BUILD_URL="https://codeload.github.com/lenny-intel/developer-scripts/zip/multi2"
-FILE_SPEC="developer-scripts-multi2/releases/nightly-build/compose-files/*"
+FILE_SPEC="developer-scripts-multi2/releases/nightly-build/compose-files/source/*"
 
 # x86_64 or arm64 for make run option
 [ "$(uname -m)" != "x86_64" ] && USE_ARM64="arm64"
@@ -11,7 +11,7 @@ FILE_SPEC="developer-scripts-multi2/releases/nightly-build/compose-files/*"
 [ "$SECURITY_SERVICE_NEEDED" != true ] && USE_NO_SECURITY="no-secty"
 
 # Download and extract all the docker compose files
-wget -O compose-files.zip ${NIGHT_BUILD_URL}
+curl -o compose-files.zip ${NIGHT_BUILD_URL}
 unzip -o -j compose-files.zip ${FILE_SPEC} -d compose-files
 
 (
