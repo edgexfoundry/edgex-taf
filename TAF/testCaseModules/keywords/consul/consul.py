564df56f6112da0738ec0a469a570d61148cf537
Ginny Guan <ginny@iotechsys.com>
2020-12-04 14:05:44 +0800

    Add back "modify_consul_config" keyword
    Fix https://github.com/edgexfoundry/edgex-taf/issues/236
    
    Signed-off-by: Ginny Guan <ginny@iotechsys.com>

---
+++ TAF/testCaseModules/keywords/consul/consul.py
@@ -0,0 +1,25 @@
+"""
+ @copyright Copyright (C) 2020 IOTech Ltd
+
+ @license SPDX-License-Identifier: Apache-2.0
+
+ @file edgex.py
+
+ @description
+    Helper functions to manage consul API
+"""
+import http.client
+from TUC.data.SettingsInfo import SettingsInfo
+
+
+def modify_consul_config(path,value):
+    conn = http.client.HTTPConnection(host=SettingsInfo().constant.BASE_URL, port=8500, timeout=5)
+    conn.request(method="PUT", url=path, body=value)
+    try:
+        r1 = conn.getresponse()
+    except Exception as e:
+        raise e
+    if int(r1.status) == 200:
+        SettingsInfo().TestLog.info("Modify consul with key {} and value {}".format(path,value))
+    else:
+        raise Exception("Fail to enable MarkPushed.")
