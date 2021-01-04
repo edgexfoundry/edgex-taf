0b285777521c8ecdf4b42570670287797f847667
Ginny Guan <ginny@iotechsys.com>
2021-01-04 09:25:08 +0800

    Update deviceprofile and reading test data
    Fix https://github.com/edgexfoundry/edgex-taf/issues/252
    1. update all deviceprofile properties type to camel case format for consistency
    2. update reading resourceName to use hyphen instead of space
    3. replace security-secrets-setup with security-bootstrapper and remove kong-migrations in deploy-edgex.sh
    
    Signed-off-by: Ginny Guan <ginny@iotechsys.com>

--- TAF/utils/scripts/docker/deploy-edgex.sh
+++ TAF/utils/scripts/docker/deploy-edgex.sh
@@ -21,19 +21,13 @@ else
   if [ "$SECURITY_SERVICE_NEEDED" = true ]; then
     docker run --rm -v ${WORK_DIR}:${WORK_DIR}:rw,z -w ${WORK_DIR} -v /var/run/docker.sock:/var/run/docker.sock \
           --env WORK_DIR=${WORK_DIR} --env PROFILE=${PROFILE} --security-opt label:disable ${COMPOSE_IMAGE} \
-          -f "${WORK_DIR}/TAF/utils/scripts/docker/docker-compose${BACKWARD}.yaml" up -d security-secrets-setup vault
+          -f "${WORK_DIR}/TAF/utils/scripts/docker/docker-compose${BACKWARD}.yaml" up -d security-bootstrapper vault
 
     sleep 20
 
     docker run --rm -v ${WORK_DIR}:${WORK_DIR}:rw,z -w ${WORK_DIR} -v /var/run/docker.sock:/var/run/docker.sock \
           --env WORK_DIR=${WORK_DIR} --env PROFILE=${PROFILE} --security-opt label:disable ${COMPOSE_IMAGE} \
           -f "${WORK_DIR}/TAF/utils/scripts/docker/docker-compose${BACKWARD}.yaml" up -d vault-worker kong-db
-
-    sleep 10
-
-    docker run --rm -v ${WORK_DIR}:${WORK_DIR}:rw,z -w ${WORK_DIR} -v /var/run/docker.sock:/var/run/docker.sock \
-          --env WORK_DIR=${WORK_DIR} --env PROFILE=${PROFILE} --security-opt label:disable ${COMPOSE_IMAGE} \
-          -f "${WORK_DIR}/TAF/utils/scripts/docker/docker-compose${BACKWARD}.yaml" up -d kong-migrations
 
     sleep 10
 
