f25c6860742d8d202da69510cc2804fc28bc5afe
lenny <leonard.goodell@intel.com>
2021-02-18 15:52:01 -0700

    refactor: Update scripts to pull compose files from edgex-compose repo
    closes #312
    
    Signed-off-by: lenny <leonard.goodell@intel.com>

---
+++ TAF/utils/scripts/docker/sync-compose-file.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# Use specified commit or master
+USE_SHA1=$1
+
+# security or no security
+USE_NO_SECURITY=$2
+
+# x86_64 or arm64
+USE_ARM64=$3
+
+TAF=$4
+TAF_PERF=$5
+PRE_RELEASE="-pre-release"
+# Handle TAF specific compose files
+[ "$TAF" = "-taf" ] && TAF_SUB_FOLDER="/taf" && PRE_RELEASE=""
+
+NIGHTLY_BUILD_URL="https://raw.githubusercontent.com/edgexfoundry/edgex-compose/${USE_SHA1}/releases/pre-release${TAF_SUB_FOLDER}"
+
+COMPOSE_FILE="docker-compose${PRE_RELEASE}${TAF}${TAF_PERF}${USE_NO_SECURITY}${USE_ARM64}.yml"
+curl -o ${COMPOSE_FILE} "${NIGHTLY_BUILD_URL}/${COMPOSE_FILE}"
