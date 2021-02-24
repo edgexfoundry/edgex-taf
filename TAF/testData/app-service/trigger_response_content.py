42e4ee744d8b8d331ac93b7f22084ae414d88ea2
Cherry Wang <cherry@iotechsys.com>
2021-02-24 13:48:20 +0800

    Remove created field for json related string from trigger_response_content.py file
    Signed-off-by: Cherry Wang <cherry@iotechsys.com>

--- TAF/testData/app-service/trigger_response_content.py
+++ TAF/testData/app-service/trigger_response_content.py
@@ -5,15 +5,15 @@ trigger_content={
                   '<ResourceName>Int32</ResourceName><ProfileName>Random-Integer-Device</ProfileName><ValueType>Int32</ValueType><BinaryValue></BinaryValue>'
                   '<MediaType></MediaType><Value>123</Value></Readings></Event>',
     "JSON String": '{"apiVersion": "v2", "id": "92eb2e26-0f24-48aa-1111-de9dac3fb903", "deviceName": "Random-Integer-Device", "profileName": "Random-Integer-Device", '
-                   '"created": 0, "origin": 1540855006469, "readings": [{"apiVersion": "v2", "id": "82eb2e26-0f24-48aa-ae4c-de9dac3fb920", "created": 0, '
+                   '"origin": 1540855006469, "readings": [{"apiVersion": "v2", "id": "82eb2e26-0f24-48aa-ae4c-de9dac3fb920", '
                    '"origin": 1540855006469, "deviceName": "Random-Integer-Device", "resourceName": "Int32", "profileName": "Random-Integer-Device", '
                    '"valueType": "Int32", "binaryValue": null, "mediaType": "", "value": "123"}]}',
-    "JSON-GZIP String": 'H4sIAAAAAAAA/4yPT0vEMBTEv8ucE0jSbmlz9rIXDyJ7EQ+vzWt50CYl/QOL7HcXRWVBxJ7nN8xv3kCzXDgvkiI8dgcFCfBoHLeOXaVN70pd1kTaWmt14CZQV/'
-                        'RtYwooBN6l40eaGB5PFEOa9DmuPHDWD58ZFOacehn/o7rMtHKANwopyyAR3p5KU59OxlRl1ShkpiBxWOBf/hSvf4sTl92duDPH1g59y7ykLf9g57gW7vDlncaNn6/'
-                        'zfbOVSPl6+Ujg4zaOChMHoS/suwUP6wrcXm/vAQAA//99EZmcwwEAAA==',
-    "JSON-ZLIB String": 'eJyMj09LxDAUxL/LnBNI0m5pc/ayFw8iexEPr81redAmJf0Di+x3F0VlQcSe5zfMb95As1w4L5IiPHYHBQnwaBy3jl2lTe9KXdZE2lprdeAmUFf0bWMKKATepeNHmhge'
-                        'TxRDmvQ5rjxw1g+fGRTmnHoZ/6O6zLRygDcKKcsgEd6eSlOfTsZUZdUoZKYgcVjgX/4Ur3+LE5fdnbgzx9YOfcu8pC3/YOe4Fu7w5Z3GjZ+v832zlUj5evlI4OM2jgoTB6'
-                        'Ev7LsFD+sK3F5v7wEAAP//fdqMmQ==',
+    "JSON-GZIP String": 'H4sIAAAAAAAA/4yPMWvDMBSE/8vNFkiybGzNXbJ0KCVL6fBsPZsHtmTk2BBK/ntpaUughGS+77jvPkCLHDmvkiI8dosCEuDRWu4s21rpwTrlGiJljDEqcBuoL4eu1SUKBN6'
+                        'l52eaGR4vFEOa1SGeeOSsnr4zFFhyGmS6R6Uso0R4UzndVJXWtavbApkpSBxX+Lebss1/WWLXX8lafXvhoQ+Z17TlP+wQT6V9+NpO08av5+W62UmkfD5+JfBxm6YCMwehH+y'
+                        '3BQ9jS1zeL58BAAD//4PojOurAQAA',
+    "JSON-ZLIB String": 'eJyMjzFrwzAUhP/LzRZIsmxszV2ydCglS+nwbD2bB7Zk5NgQSv57aWlLoIRkvu+47z5Aixw5r5IiPHaLAhLg0VruLNta6cE65RoiZYwxKnAbqC+HrtUlCgTepednmhkeLx'
+                        'RDmtUhnnjkrJ6+MxRYchpkukelLKNEeFM53VSV1rWr2wKZKUgcV/i3m7LNf1li11/JWn174aEPmde05T/sEE+lffjaTtPGr+flutlJpHw+fiXwcZumAjMHoR/stwUPY0tc3'
+                        'i+fAQAA///LG4U1',
     "JSON-ZLIB-AES String": '+dKkxw4xiGqISkUTODQOzzS3Zjz2+N6L1qwVWeTuhiQ3DcDWLxp1OkO6uvshoKDoxwQCQv+omF8PdFOIO821aLV8ZXAlxud7O7rFhBdi7jvUSFdjNEUF6SVEHxehxz'
                             'Tyr6eNLodEbCgob+dz1/kOZNIqlLdoAcirFcPuSRYGBNm+U/Txg8/VwN+JpZeOTyV+2ZIu1AYEwvB+QKKtzEPk3xCBvkum/i+2krzsn6NMx7ObSMz37DfkZGC90THvc5'
                             'FGU2NmwuFot6CPp2M3fSaFm1MOZ6ucqHmdxY4MvRknmmSTN2Q51Ks3nUQnDWGIBTJkDnGZ+Sa2JewH+IkmIUpp4tIk4rulj9bKpFngBhgAcMKXyqoXtYWxXpacBtCA9qc'
