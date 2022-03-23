7b8d80b61c04d5d4811f65c845ed4e61dac34dfd
Cherry Wang <cherry@iotechsys.com>
2022-03-23 13:44:11 +0800

    fix: Replace MQTT_BROKER_IP with MQTT_BROKER_HOSTNAME
    Signed-off-by: Cherry Wang <cherry@iotechsys.com>

--- TAF/utils/scripts/docker/exec_performance_metrics.sh
+++ TAF/utils/scripts/docker/exec_performance_metrics.sh
@@ -2,7 +2,7 @@
 # set default values
 USE_ARCH=${1:-x86_64}
 USE_SECURITY=${2:--}
-USE_SHA1=${3:-master}
+USE_SHA1=${3:-main}
 
 # # x86_64 or arm64
 [ "$USE_ARCH" = "arm64" ] && USE_ARM64="-arm64"
@@ -23,6 +23,7 @@ sleep 5
 # Run scripts to collect performance metrics and generate reports
 docker run --rm --network host --privileged -v ${WORK_DIR}:${WORK_DIR}:z -w ${WORK_DIR} -e ARCH=${USE_ARCH} \
        -v /var/run/docker.sock:/var/run/docker.sock -e SECURITY_SERVICE_NEEDED=${SECURITY_SERVICE_NEEDED} \
+       --env-file ${WORK_DIR}/TAF/utils/scripts/docker/common-taf.env \
        -e COMPOSE_IMAGE=${COMPOSE_IMAGE} ${TAF_COMMON_IMAGE} \
        --exclude Skipped -u performanceTest/performance-metrics-collection --profile performance-metrics
 
--- TAF/utils/scripts/docker/get-compose-file-perfermance.sh
+++ TAF/utils/scripts/docker/get-compose-file-perfermance.sh
@@ -2,7 +2,7 @@
 # # set default values
 USE_ARCH=${1:-x86_64}
 USE_SECURITY=${2:--}
-USE_SHA1=${3:-master}
+USE_SHA1=${3:-main}
 
 # # x86_64 or arm64
 [ "$USE_ARCH" = "arm64" ] && USE_ARM64="-arm64"
@@ -25,5 +25,5 @@ fi
 # # Then sync TAF performance specific docker-compose file from edgex-compose repo and replace the placeholders
 ./sync-compose-file.sh "${USE_SHA1}" "${USE_NO_SECURITY}" "${USE_ARM64}" "-taf" "-perf"
 cp docker-compose-taf-perf${USE_NO_SECURITY}${USE_ARM64}.yml docker-compose-mqtt.yml
-sed -i 's/\MQTT_BROKER_ADDRESS_PLACE_HOLDER/edgex-mqtt-broker/g' docker-compose-mqtt.yml
+sed -i 's/\MQTT_BROKER_ADDRESS_PLACE_HOLDER/${MQTT_BROKER_HOSTNAME}/g' docker-compose-mqtt.yml
 sed -i 's/\LOGLEVEL: INFO/LOGLEVEL: DEBUG/g' docker-compose-mqtt.yml
