0d4213eef12bba002415f56633c7a1ebb86564a0
Cherry Wang <cherry@iotechsys.com>
2022-09-13 10:32:51 +0800

    feat: Generate compose file for Delayed Start test
    Signed-off-by: Cherry Wang <cherry@iotechsys.com>

--- TAF/utils/scripts/docker/get-compose-file.sh
+++ TAF/utils/scripts/docker/get-compose-file.sh
@@ -5,6 +5,7 @@ USE_ARCH=${1:--x86_64}
 USE_SECURITY=${2:--}
 USE_SHA1=${3:-main}
 TEST_STRATEGY=${4:-}
+DELAYED_START=${5:-false}
 
 # # x86_64 or arm64
 [ "$USE_ARCH" = "arm64" ] && USE_ARM64="-arm64"
@@ -35,9 +36,31 @@ for compose in ${COMPOSE_FILE}; do
     sed -n "/ ${profile}:/,/ - PROFILE_VOLUME_PLACE_HOLDER/ p" ${compose}.yml > tmp/${profile}.yml  # print device-service
     sed -i 's/\CONF_DIR_PLACE_HOLDER/${CONF_DIR}/g' tmp/${profile}.yml
     sed -i "s/\PROFILE_VOLUME_PLACE_HOLDER/\${WORK_DIR}\/TAF\/config\/${profile}/g" tmp/${profile}.yml
+    # Enable Delayed Start
+    if [ "${TEST_STRATEGY}" = "integration-test" ] && [ "${USE_SECURITY}" = '-security-' ] \
+      && [ "${DELAYED_START}" = 'true' ]; then
+      sed -i '/\ \ \ \ environment:/a \ \ \ \ \ \ SECRETSTORE_RUNTIMETOKENPROVIDER_ENABLED: "true"' tmp/${profile}.yml
+      sed -i '/\ \ \ \ environment:/a \ \ \ \ \ \ SECRETSTORE_RUNTIMETOKENPROVIDER_HOST: edgex-security-spiffe-token-provider' tmp/${profile}.yml
+      sed -i '/\ \ volumes:/a \ \ \ \ - \/tmp\/edgex\/secrets\/spiffe\/public:\/tmp\/edgex\/secrets\/spiffe\/public:ro,z' tmp/${profile}.yml
+      sed -i "/tmp\/edgex\/secrets\/${profile}/d" tmp/${profile}.yml
+    fi
     sed -i "/ ${profile}:/,/ - PROFILE_VOLUME_PLACE_HOLDER/d" ${compose}.yml
     sed -i "/services:/ r tmp/${profile}.yml" ${compose}.yml
   done
+
+  # Enable Delayed Start
+  if [ "${TEST_STRATEGY}" = "integration-test" ] && [ "${USE_SECURITY}" = '-security-' ] \
+        && [ "${DELAYED_START}" = 'true' ]; then
+    for service in notifications scheduler; do
+      sed -n "/^\ \ ${service}:/,/^  [a-z].*:$/p" ${compose}.yml | sed '$d' > tmp/${service}.yml
+      sed -i '/\ \ \ \ environment:/a \ \ \ \ \ \ SECRETSTORE_RUNTIMETOKENPROVIDER_ENABLED: "true"' tmp/${service}.yml
+      sed -i '/\ \ \ \ environment:/a \ \ \ \ \ \ SECRETSTORE_RUNTIMETOKENPROVIDER_HOST: edgex-security-spiffe-token-provider' tmp/${service}.yml
+      sed -i '/\ \ volumes:/a \ \ \ \ - \/tmp\/edgex\/secrets\/spiffe\/public:\/tmp\/edgex\/secrets\/spiffe\/public:ro,z' tmp/${service}.yml
+      sed -i "/tmp\/edgex\/secrets\/support-${service}/d" tmp/${service}.yml
+      sed -i "/^\ \ ${service}:/,/^  [a-z].*:$/{//!d}; /^\ \ ${service}:/d" ${compose}.yml
+      sed -i "/services:/ r tmp/${service}.yml" ${compose}.yml
+    done
+  fi
 
   sed -i '/PROFILE_VOLUME_PLACE_HOLDER: {}/d' ${compose}.yml
   sed -i 's/\EXPORT_HOST_PLACE_HOLDER/${DOCKER_HOST_IP}/g' ${compose}.yml
