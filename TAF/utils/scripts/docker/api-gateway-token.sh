002fd11df7111611d682dc0424280e64b324998f
Bryon Nevis <bryon.nevis@intel.com>
2023-03-08 16:43:31 -0800

    fix: Use EdgeX 3.0 method to get API gateway token
    Signed-off-by: Bryon Nevis <bryon.nevis@intel.com>

--- TAF/utils/scripts/docker/api-gateway-token.sh
+++ TAF/utils/scripts/docker/api-gateway-token.sh
@@ -5,43 +5,21 @@ PROXY_IMAGE=$(docker inspect --format='{{.Config.Image}}' edgex-security-proxy-s
 PROXY_NETWORK_ID=$(docker inspect --format='{{range .NetworkSettings.Networks}}{{.NetworkID}}{{end}}' edgex-security-proxy-setup)
 
 # Command vars
-USER="gateway"
-GROUP="gateway-group"
-
-# JWT File
-JWT_FILE=/tmp/edgex/secrets/security-proxy-setup/kong-admin-jwt
-JWT_VOLUME=/tmp/edgex/secrets/security-proxy-setup
-
-if [ ! -f "${WORK_DIR}/${USER}.pub" ];
-then
-  openssl ecparam -name prime256v1 -genkey -noout -out ${WORK_DIR}/${USER}.key 2> /dev/null
-  openssl ec -in ${WORK_DIR}/${USER}.key -pubout -out ${WORK_DIR}/${USER}.pub 2> /dev/null
-fi
+USER="tafuser"
 
 case ${option} in
   -useradd)
-    # Create a user in Kong
-    ID=`cat /proc/sys/kernel/random/uuid 2> /dev/null` || ID=`uuidgen`
-    docker run --rm -e KONGURL_SERVER=edgex-kong -e "ID=${ID}" -e "JWT_FILE=${JWT_FILE}" -e "USER=${USER}" \
-           -e "GROUP=${GROUP}" --network=${PROXY_NETWORK_ID} --entrypoint "" -v ${WORK_DIR}:/keys -v ${JWT_VOLUME}:${JWT_VOLUME} ${PROXY_IMAGE} \
-           /bin/sh -c 'JWT=`cat ${JWT_FILE}`; /edgex/secrets-config proxy adduser --jwt ${JWT} --token-type jwt --id ${ID} \
-           --algorithm ES256 --public_key /keys/${USER}.pub --user ${USER} --group ${GROUP} > /dev/null'
-
-    # Create JWT and associate with Kong user in previous step
-    docker run --rm -e KONGURL_SERVER=edgex-kong -e "ID=${ID}" -e "USER=${USER}" --network=${PROXY_NETWORK_ID} --entrypoint "" \
-           -v ${WORK_DIR}:/keys ${PROXY_IMAGE} /bin/sh -c '/edgex/secrets-config proxy \
-           jwt --algorithm ES256 --id ${ID} --private_key /keys/${USER}.key'
+    password=$(docker run --rm -e "USER=${USER}" --network=${PROXY_NETWORK_ID} -v edgex_vault-config:/vault/config:ro -e SECRETSTORE_HOST=edgex-vault --entrypoint /edgex/secrets-config ${PROXY_IMAGE} proxy adduser --user ${USER} -useRootToken | jq -r '.password')
+    vault_token=$(curl -ks "http://localhost:8200/v1/auth/userpass/login/${USER}" -d "{\"password\":\"${password}\"}" | jq -r '.auth.client_token')
+    id_token=$(curl -ks -H "Authorization: Bearer ${vault_token}" "http://localhost:8200/v1/identity/oidc/token/${USER}" | jq -r '.data.token')
+    echo "${id_token}"
   ;;
   -userdel)
-    # Remove a user from Kong
-    docker run --rm -e KONGURL_SERVER=edgex-kong -e "JWT_FILE=${JWT_FILE}" -e "USER=${USER}" --network=${PROXY_NETWORK_ID} --entrypoint "" \
-           -v ${WORK_DIR}:/keys -v ${JWT_VOLUME}:${JWT_VOLUME} ${PROXY_IMAGE} /bin/sh -c 'JWT=`cat ${JWT_FILE}`; /edgex/secrets-config proxy \
-           deluser --user ${USER} --jwt ${JWT} > /dev/null'
+    # Remove a user from proxy
+    docker run --rm -e "USER=${USER}" --network=${PROXY_NETWORK_ID} -v edgex_vault-config:/vault/config:ro -e SECRETSTORE_HOST=edgex-vault --entrypoint /edgex/secrets-config ${PROXY_IMAGE} \
+      proxy deluser --user ${USER} -useRootToken > /dev/null
   ;;
   *)
     exit 0
   ;;
 esac
-
-# Clean up
-rm ${WORK_DIR}/${USER}*
--- TAF/utils/scripts/snap/api-gateway-token.sh
+++ TAF/utils/scripts/snap/api-gateway-token.sh
@@ -2,39 +2,23 @@
 
 option=${1}
 # Command vars
-USER="gateway"
-GROUP="gateway-group"
+USER="tafuser"
 
 . "$SCRIPT_DIR/snap-utils.sh"
 
 snap_maybe_switch_asc_profile
  
-# JWT File
-JWT_FILE=/var/snap/edgexfoundry/current/secrets/security-proxy-setup/kong-admin-jwt
-JWT_VOLUME=/var/snap/edgexfoundry/current/secrets/security-proxy-setup
-JWT=`cat ${JWT_FILE}`
-
-mkdir -p ${WORK_DIR}/keys
-
-if [ ! -f "${WORK_DIR}/keys/${USER}.pub" ];
-then
-  openssl ecparam -name prime256v1 -genkey -noout -out ${WORK_DIR}/keys/${USER}.key > /dev/null 2>&1
-  openssl ec -in ${WORK_DIR}/keys/${USER}.key -pubout -out ${WORK_DIR}/keys/${USER}.pub > /dev/null 2>&1
-fi
-
 case ${option} in
   -useradd)
-    # Create a user in Kong
-    ID=`cat /proc/sys/kernel/random/uuid 2> /dev/null` || ID=`uuidgen`
-    edgexfoundry.secrets-config proxy adduser --jwt ${JWT} --token-type jwt --id ${ID} \
-           --algorithm ES256 --public_key ${WORK_DIR}/keys/${USER}.pub --user ${USER} --group ${GROUP}  > /dev/null 2>&1
-
-    # Create JWT and associate with Kong user in previous step. This script returns the token in stdout
-    edgexfoundry.secrets-config proxy jwt --algorithm ES256 --id ${ID} --private_key ${WORK_DIR}/keys/${USER}.key 2> /dev/null
+    # Create new user, log in, and exchange for JWT
+    password=$(edgexfoundry.secrets-config proxy adduser --user ${USER} --useRootToken | jq -r '.password')
+    vault_token=$(curl -ks "http://localhost:8200/v1/auth/userpass/login/${username}" -d "{\"password\":\"${password}\"}" | jq -r '.auth.client_token')
+    id_token=$(curl -ks -H "Authorization: Bearer ${vault_token}" "http://localhost:8200/v1/identity/oidc/token/${username}" | jq -r '.data.token')
+    echo "${id_token}"
   ;;
   -userdel)
     # Remove a user from Kong
-    edgexfoundry.secrets-config proxy deluser --user ${USER} --jwt ${JWT} > /dev/null 2>&1
+    edgexfoundry.secrets-config proxy deluser --user ${USER} --useRootToken > /dev/null 2>&1
   ;;
   *)
     exit 0
@@ -43,4 +27,3 @@ esac
 
 
 >&2 echo "INFO:snap-TAF: api-gateway-token done"
-
