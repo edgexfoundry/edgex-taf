c8b8589090931e1a64bccb38978c47e66035b5f6
Cherry Wang <cherry@iotechsys.com>
2023-03-29 12:00:40 +0800

    refactor: Change configuration format from TOML to YAML and update device profile minimum and maximum resource properties
    Signed-off-by: Cherry Wang <cherry@iotechsys.com>

--- TAF/utils/scripts/docker/deploy-edgex.sh
+++ TAF/utils/scripts/docker/deploy-edgex.sh
@@ -16,11 +16,12 @@ else
            docker compose -f "${WORK_DIR}/TAF/utils/scripts/docker/docker-compose.yml" up --no-start --no-deps ${PROFILE}
 
     # copy device service default configuration in the res to TAF/config/{service}/res
-    docker cp edgex-${PROFILE}:/res/configuration.toml ${WORK_DIR}/TAF/config/${PROFILE}
-    sed -i "s/\[Device\]/[Device_old]/" ${WORK_DIR}/TAF/config/${PROFILE}/configuration.toml
-    echo "" >> ${WORK_DIR}/TAF/config/${PROFILE}/configuration.toml
-    echo "[Device]" >> ${WORK_DIR}/TAF/config/${PROFILE}/configuration.toml
-    echo "ProfilesDir = \"$CONFIG_DIR\"" >> ${WORK_DIR}/TAF/config/${PROFILE}/configuration.toml
+    docker cp edgex-${PROFILE}:/res/configuration.yaml ${WORK_DIR}/TAF/config/${PROFILE}
+    sed -i "s/Device:/Device_old:/" ${WORK_DIR}/TAF/config/${PROFILE}/configuration.yaml
+    echo "" >> ${WORK_DIR}/TAF/config/${PROFILE}/configuration.yaml
+    echo "Device:" >> ${WORK_DIR}/TAF/config/${PROFILE}/configuration.yaml
+    echo "  ProfilesDir: \"$CONFIG_DIR\"" >> ${WORK_DIR}/TAF/config/${PROFILE}/configuration.yaml
+    echo "  DevicesDir: \"$CONFIG_DIR\"" >> ${WORK_DIR}/TAF/config/${PROFILE}/configuration.yaml
 
   done
 
