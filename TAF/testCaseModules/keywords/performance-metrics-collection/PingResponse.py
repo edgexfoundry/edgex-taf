b539e7ac0b2a328ca25afbe9860e9502c0a89afc
Cherry Wang <cherry@iotechsys.com>
2023-04-26 16:56:14 +0800

    fix: Update trigger_content to use api version v3
    Signed-off-by: Cherry Wang <cherry@iotechsys.com>

--- TAF/testCaseModules/keywords/performance-metrics-collection/PingResponse.py
+++ TAF/testCaseModules/keywords/performance-metrics-collection/PingResponse.py
@@ -31,11 +31,10 @@ def send_ping_request(port, token):
     else:
         service_url = "{}://{}:{}".format(SettingsInfo().constant.URI_SCHEME, SettingsInfo().constant.BASE_URL, port)
         requests.packages.urllib3.disable_warnings()  # disable ssl-warning
-        api_version = "{}".format(SettingsInfo().constant.API_VERSION)
-    ping_url = "{}/api/{}/ping".format(service_url, api_version)
+    ping_url = "{}/api/{}/ping".format(service_url, SettingsInfo().constant.API_VERSION)
     header = {"Authorization": "Bearer {}".format(token)}
     res = requests.get(ping_url, headers=header, verify=False)
-    logger.info("response body: {}, reponse time: {}".format(res.content, res.elapsed.total_seconds()))
+    logger.info("response body: {}, response time: {}".format(res.content, res.elapsed.total_seconds()))
     response = {
                 "body": res.content.decode("utf-8"),
                 "seconds": res.elapsed.total_seconds()
